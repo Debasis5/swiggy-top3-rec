85f272fd5de3f44d184f7f65af80fe6e3abfd468
Debasis Patra <debasispatra.sbd@gmail.com>
2023-11-26 20:49:12 +0530

    inital cookie-cutter template code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will recommend the top3 food items on swiggy',
+    author='Debasis Patra',
+    license='',
+)
